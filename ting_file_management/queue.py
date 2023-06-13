b73f2f2486de67c1373397b7c73938bfdfdca6fe
victrosantos <victor.santos.fk@hotmail.com>
2023-06-13 14:48:28 -0300

    feat: Feito fila e suas funçoes

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,17 +3,35 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
-        # aaaaa
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return 0
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return None
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if len(self._data) <= 0 or (index
+                                    is None) or not (0 <= index
+                                                     < len(
+                                                         self._data)
+                                                     ) or not isinstance(
+                index, int):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
+
+
+if __name__ == "__main__":
+    testeQ = Queue()
+    # testeQ.enqueue(3)
+    # testeQ.enqueue(7)
+    # testeQ.enqueue(5)
+    testeQ.search(len(testeQ))
