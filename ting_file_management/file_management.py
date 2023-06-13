40f62267fe23d0e969c35e1526f89202e80d77da
victrosantos <victor.santos.fk@hotmail.com>
2023-06-13 15:03:40 -0300

    feat: Feito leitor de arquivo txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return None
+    try:
+        with open(path_file, 'r') as arquivo:
+            linhas = [linha.strip() for linha in arquivo.readlines()]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+
+    print(linhas)
+    return linhas
+
+
+if __name__ == "__main__":
+    txt_importer("statics/arquivo_teste.txt")
