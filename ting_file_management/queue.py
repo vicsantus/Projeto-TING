399348d4e7f21a308ff4406606f1c48293858a9d
victrosantos <victor.santos.fk@hotmail.com>
2023-06-13 14:01:58 -0300

    feat: Iniciando o projeto

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,6 +4,7 @@ from ting_file_management.abstract_queue import AbstractQueue
 class Queue(AbstractQueue):
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        # aaaaa
 
     def __len__(self):
         """Aqui irá sua implementação"""
