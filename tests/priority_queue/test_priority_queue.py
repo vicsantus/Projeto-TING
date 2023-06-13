faf7af610ff29c631940e40f28a479eb0038580e
victrosantos <victor.santos.fk@hotmail.com>
2023-06-13 17:49:05 -0300

    feat: Feito teste da função priority_queue.py

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,17 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    projeto = PriorityQueue()
+    projeto.enqueue({"valor": 'Jorgin', "qtd_linhas": 16})
+    projeto.enqueue({"valor": 'Setinha', "qtd_linhas": 5})
+    assert len(projeto) == 2
+    projeto.dequeue()
+    assert len(projeto) == 1
+    assert projeto.search(0) == {"valor": 'Setinha', "qtd_linhas": 5}
+    projeto.enqueue({"valor": 'Setinha', "qtd_linhas": 2})
+    projeto.enqueue({"valor": 'Eli', "qtd_linhas": 1})
+    assert projeto.search(2) == {"valor": 'Setinha', "qtd_linhas": 5}
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        projeto.search(-1)
